7331ff4a22a57cb627beb932f889988313f24e7d
UtkarshK3099 <ut20243753@wipro.com>
2021-09-22 20:26:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ebt5z3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ebt5z3)
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./myindex.css";
+
+//import App from "./01_welcome/App"
+//import App from "./02_componentReuse/App"
+//import App from "./03_properties/App"
+//import App from "./04_events/App"
+import App from "./05_state/App"
+ReactDOM.render(<App />, document.getElementById("root"));
